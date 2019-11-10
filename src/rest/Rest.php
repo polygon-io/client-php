22e603473cef0e960017e3566fed9ab0a2264ad0
Julien Prugne <julien.prugne@gmail.com>
2019-11-10 09:18:14 +0100

    implement forex api

--- src/rest/Rest.php
+++ src/rest/Rest.php
@@ -1,10 +1,14 @@
 <?php
 namespace PolygonIO\rest;
 
+use PolygonIO\rest\forex\Forex;
 use PolygonIO\rest\reference\Reference;
+use PolygonIO\rest\stocks\Stocks;
 
 class Rest {
     public $reference;
+    public $stocks;
+    public $forex;
 
     /**
      * Rest constructor.
@@ -13,5 +17,7 @@ class Rest {
     public function __construct($apiKey)
     {
         $this->reference = new Reference($apiKey);
+        $this->stocks = new Stocks($apiKey);
+        $this->forex = new Forex($apiKey);
     }
 }
