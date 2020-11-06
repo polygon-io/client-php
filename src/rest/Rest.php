eec05e16bb3ef9e0b6a208b6ed0cc9b5eda5ae62
Mike P. Sinn <m@quantimodo.com>
2020-11-06 16:53:51 -0600

    Fixed Rest.php for use as a library

--- src/rest/Rest.php
+++ src/rest/Rest.php
@@ -1,6 +1,6 @@
 <?php
 namespace PolygonIO\rest;
-require_once __DIR__.'/../../vendor/autoload.php';
+//require_once __DIR__.'/../../vendor/autoload.php';
 
 use PolygonIO\rest\crypto\Crypto;
 use PolygonIO\rest\forex\Forex;
